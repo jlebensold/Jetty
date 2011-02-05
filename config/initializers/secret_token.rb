825b9530f1dd37f390340984e272094e8e577425
Jon Lebensold <jon@lebensold.ca>
2011-02-05 13:02:31 +0200

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jetty::Application.config.secret_token = '0b33bfb11416843ac0988c8a8ecf02fc43e1e8e081c0e97ad74ad3bcc6dfd35a2ff4ad89091cfc4de9108c659465793ebc9162537f4c33ac443b5a674be8f00b'
