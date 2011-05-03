9660ec2e69a0b8c8a56df62d6d124de4bd5b56ef
Jon Lebensold <jon@lebensold.ca>
2011-05-03 21:53:55 +0300

    styling the iframe

--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -3,9 +3,11 @@ function init(bp)
 {
     basepath = bp;
     $(".play").live('click',play_content);
+    $(".courselist .play").live('click',set_upnext);
+
     $(".playlist").tabs();    
     $(".buy").click(buy_click);
-    $(".playlist .play").first().click();
+    $(".courselist .play").first().click();
     $(".ddl").click(function(e)
     {
         $($(this).attr('rel')).toggle();
@@ -38,9 +40,21 @@ function buy_click(e)
     $("#buy").submit();
     e.preventDefault();
 }
+function set_upnext(evt)
+{
+    var next = $(this).parents('li').next();
+    if (next.length == 0) 
+        $(".upnext").text('');
+    else
+        $(".upnext").text('Next: ' + next.find('.play').text().split('$')[0]);
+    
 
+    //evt.preventDefault();
+}
 function play_content(evt)
 {
+    $('#courselist').find('.nowplaying').removeClass('nowplaying');
+    $(this).parent().addClass('nowplaying');
     $.ajax({
         url: basepath + '/../contents/show/'+$(this).attr('cid'),
         type: 'GET',
