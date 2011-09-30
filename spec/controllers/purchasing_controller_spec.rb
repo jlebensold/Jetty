8b8075acb85760e1e01ab46a93ef4e309dd7453a
Jon Lebensold <jon@lebensold.ca>
2011-09-30 13:01:53 +0300

    added spec stub

---
+++ spec/controllers/purchasing_controller_spec.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PurchasingController, " handling someaction" do  
+  it "should accept valid IPN notifications" do
+     
+  end
+
+end
